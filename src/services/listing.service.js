1dc9c83245db36b27fccc2b5d4fc6726ca7903cd
CannyRo <whatcannydev@gmail.com>
2025-04-14 14:42:39 +0200

    Fix listing service

--- src/services/listing.service.js
+++ src/services/listing.service.js
@@ -1,16 +1,4 @@
-import axios from "axios";
-
-const baseURL = import.meta.env.VITE_API_URL;
-const api = axios.create({ baseURL: baseURL || "http://localhost:5005" });
-
-// Add Authorization header for all requests
-api.interceptors.request.use((config) => {
-  const storedToken = localStorage.getItem("authToken");
-  if (storedToken) {
-    config.headers.Authorization = `Bearer ${storedToken}`;
-  }
-  return config;
-});
+import api from "./api"; 
 
 // Create a listing
 export async function createListing(listingData) {
