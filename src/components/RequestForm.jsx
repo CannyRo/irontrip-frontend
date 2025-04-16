b2a8c00c1274a5b103c466e4df40153ab3da8b91
CannyRo <whatcannydev@gmail.com>
2025-04-16 12:08:33 +0200

    New feature with cloudinary to upload image for the user - maybe for the listings later

--- src/components/RequestForm.jsx
+++ src/components/RequestForm.jsx
@@ -1,8 +1,13 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 
-export const RequestForm = ({ onSubmit, onDelete, isUpdateForm = false, initialData = null }) => {
-  console.log("isUpdateForm === ",isUpdateForm);
+export const RequestForm = ({
+  onSubmit,
+  onDelete,
+  isUpdateForm = false,
+  initialData = null,
+}) => {
+  console.log("isUpdateForm === ", isUpdateForm);
 
   const getFormattedDate = (date) => new Date(date).toISOString().split("T")[0];
 
@@ -17,17 +22,17 @@ export const RequestForm = ({ onSubmit, onDelete, isUpdateForm = false, initialD
 
   const { user } = useContext(AuthContext);
 
-  useEffect(()=>{
-    if(isUpdateForm && initialData){
+  useEffect(() => {
+    if (isUpdateForm && initialData) {
       setArrival(getFormattedDate(initialData.arrivalDate));
       setDeparture(getFormattedDate(initialData.departureDate));
       setMessageRequest(initialData.messageToHost || "");
       setStatusRequest(initialData.status || "Pending");
 
-      console.log("user?.id == ",user?.id);
-      console.log("initialData?.host?.id == ", initialData?.host?.id);
+      // console.log("user?.id == ",user?.id);
+      // console.log("initialData?.host?.id == ", initialData?.host?.id);
     }
-  },[isUpdateForm, initialData])
+  }, [isUpdateForm, initialData]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -42,7 +47,7 @@ export const RequestForm = ({ onSubmit, onDelete, isUpdateForm = false, initialD
 
   const handleDelete = () => {
     onDelete();
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -70,51 +75,86 @@ export const RequestForm = ({ onSubmit, onDelete, isUpdateForm = false, initialD
           value={messageRequest}
           onChange={(e) => setMessageRequest(e.target.value)}
           required
-        >{messageRequest}</textarea>
+        >
+          {messageRequest}
+        </textarea>
       </label>
-      {(isUpdateForm === true && user?.id === initialData?.host?._id) && (
+      {isUpdateForm === true && user?.id === initialData?.host?._id && (
         <div>
-        <label>Request status:</label>
-        <div style={{display: "flex",flexDirection: "column", gap: "8px", padding: "12px", border: "1px solid #ccc", borderRadius: "5px"}}>
-          <label style={{display: "flex", flexDirection: "row", gap: "2rem", justifyContent: "space-between"}}>
-          Pending
-          <input
-            type="radio"
-            value="Pending"
-            checked={statusRequest === "Pending"}
-            onChange={(e) => setStatusRequest(e.target.value)}
-            style={{width: "10rem"}}
-          />
-        </label>
+          <label>Request status:</label>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "8px",
+              padding: "12px",
+              border: "1px solid #ccc",
+              borderRadius: "5px",
+            }}
+          >
+            <label
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                gap: "2rem",
+                justifyContent: "space-between",
+              }}
+            >
+              Pending
+              <input
+                type="radio"
+                value="Pending"
+                checked={statusRequest === "Pending"}
+                onChange={(e) => setStatusRequest(e.target.value)}
+                style={{ width: "10rem" }}
+              />
+            </label>
 
-        <label style={{display: "flex", flexDirection: "row", gap: "2rem", justifyContent: "space-between"}}>
-          Accepted
-          <input
-            type="radio"
-            value="Accepted"
-            checked={statusRequest === "Accepted"}
-            onChange={(e) => setStatusRequest(e.target.value)}
-            style={{width: "10rem"}}
-          />
-        </label>
+            <label
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                gap: "2rem",
+                justifyContent: "space-between",
+              }}
+            >
+              Accepted
+              <input
+                type="radio"
+                value="Accepted"
+                checked={statusRequest === "Accepted"}
+                onChange={(e) => setStatusRequest(e.target.value)}
+                style={{ width: "10rem" }}
+              />
+            </label>
 
-        <label style={{display: "flex", flexDirection: "row", gap: "2rem", justifyContent: "space-between"}}>
-          Rejected
-          <input
-            type="radio"
-            value="Rejected"
-            checked={statusRequest === "Rejected"}
-            onChange={(e) => setStatusRequest(e.target.value)}
-            style={{width: "10rem"}}
-          />
-        </label>
+            <label
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                gap: "2rem",
+                justifyContent: "space-between",
+              }}
+            >
+              Rejected
+              <input
+                type="radio"
+                value="Rejected"
+                checked={statusRequest === "Rejected"}
+                onChange={(e) => setStatusRequest(e.target.value)}
+                style={{ width: "10rem" }}
+              />
+            </label>
+          </div>
         </div>
-        
-      </div>
       )}
-      
+
       <button type="submit">{isUpdateForm ? "Update" : "Submit"}</button>
-      {(isUpdateForm === true && user?.id === initialData?.traveler?._id) && <button type="button" onClick={handleDelete}>Delete</button>}
+      {isUpdateForm === true && user?.id === initialData?.traveler?._id && (
+        <button type="button" onClick={handleDelete}>
+          Delete
+        </button>
+      )}
     </form>
   );
 };
