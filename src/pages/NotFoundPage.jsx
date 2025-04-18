f8d506c6f7e49027d6c9441a7da477b1f2f130d7
CannyRo <whatcannydev@gmail.com>
2025-04-18 10:45:58 +0200

    Page Not Found Style

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import Lottie from "lottie-react";
+import animationData from "../assets/AnimationNotFound.json";
+import { Link } from "react-router";
 
 export const NotFoundPage = () => {
   return (
     <main>
-      <div>NotFoundPage</div>
+      <div style={{ width: 300, height: 300 }}>
+        <Lottie animationData={animationData} autoplay loop={true} />
+      </div>
+      <h1 style={{fontSize: "2rem"}}>Page Not Found</h1>
+      <p style={{marginBottom: "2rem"}}>Ooops 🤭</p>
+      <Link to="/" style={{color: "var(--primary-color-ligth)"}}>Back to Home </Link>
     </main>
-    
-  )
-}
+  );
+};
