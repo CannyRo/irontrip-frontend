d52012494d9b041ed3ef54b720fd857618cf3ed6
CannyRo <whatcannydev@gmail.com>
2025-04-18 10:31:05 +0200

    About page style

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,49 +1,48 @@
 import React from "react";
-import "../App.css"; 
+import "../App.css";
 
 export const AboutPage = () => {
   return (
     <main>
-      <div className="about-page">
-      <h1>About IronTrip App</h1>
-      <section className="project-description">
-        <p>
-          IronTrip is a web application designed to help users plan their trips
-          and discover new destinations. The app allows users to create and
-          manage their travel itineraries, including adding destinations,
-          activities, and accommodations. Users can also search for popular
-          destinations and view travel tips and recommendations from other
-          travelers.
-        </p>
-      </section>
-      <section className="team-section">
-        <h2>Meet the Team</h2>
-        <div className="developer-profiles">
-          <div className="developer">
-            <h3>Ronan</h3>
-            <a
-              href="https://github.com/CannyRo"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </a>
+      <div className="about-container">
+        <h1 className="about-title">About IronTrip App</h1>
+        <section className="about-description">
+          <p>
+            IronTrip is a web application designed to help users plan their
+            trips and discover new destinations. In the end, the application
+            will enable users to create and manage their travel itineraries,
+            including adding destinations, activities and accommodations. Users
+            could also search for popular destinations and consult tips and
+            recommendations from other travelers. The current feature is focus
+            on creating a profile, listings and booking some dates.
+          </p>
+        </section>
+        <section className="about-team">
+          <h2>Meet the Team</h2>
+          <div className="developer-profiles">
+            <div className="developer">
+              <h3>Ronan</h3>
+              <a
+                href="https://github.com/CannyRo"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+            </div>
+            <div className="developer">
+              <h3>Enrique</h3>
+              <a
+                href="https://github.com/ecastanedam"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+            </div>
           </div>
-          <div className="developer">
-            <h3>Enrique</h3>
-            <a
-              href="https://github.com/ecastanedam"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </a>
-          </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
     </main>
-    
   );
 };
-
